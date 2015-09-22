05bc176c682776175b4f3ce9e1988888b605f97b
SBreezyx <lugia584@gmail.com>
2015-09-22 17:59:01 +1000

    More mapping done
    I've simplified the map down to just a 3D array of connections (with
    transport type), and written most of the operation functions on it
    except for DFS and BFS. hopefully will complete those tonight

--- Map.h
+++ Map.h
@@ -5,72 +5,27 @@
 
 #include "Places.h"
 
-<<<<<<< HEAD
-typedef struct vNode *vertex;
-
-struct vNode {
-	LocationID  v;    // ALICANTE, etc
-	int vDegree		//tells how many edges incident with this vertex, for use in euler pathing/tours 
-};
-
-typedef struct edgeType{
-	TransportID  road;
-	TransportID  rail;
-	TransportID boat;
-} EdgeType;
+typedef unsigned char bool
 
 struct MapRep {
 	int   nV;         // #vertices
 	int   nE;         // #edges
-	EdgeType **edges  //adjacency matrix of edges
-	vertex *locations //use this to store useful things like maybe vertex degree?
+	bool edges[NUM_MAP_LOCATIONS][NUM_MAP_LOCATIONS][MAX_TRANSPORT+1]  //adjacency matrix of edges containing transport type
+	//vertex *locations //could use this to store useful things like maybe vertex degree?
 };
-=======
-typedef struct vNode *VList;
-
-struct vNode {
-	LocationID  v;    // ALICANTE, etc
-	TransportID type; // ROAD, RAIL, BOAT
-	VList       next; // link to next node
-};
-
-struct MapRep {
-	int   nV;         // #vertices
-	int   nE;         // #edges
-	VList connections[NUM_MAP_LOCATIONS]; // array of lists
-};
-
-
-typedef struct edge{
-	LocationID  start;
-	LocationID  end;
-	TransportID type;
-} Edge;
->>>>>>> Biendeo/master
-
 // graph representation is hidden 
 typedef struct MapRep *Map; 
 
 // operations on graphs 
-<<<<<<< HEAD
 Map newMap (void);  
-void disposeMap (Map g); 
-void showMap (Map g); 
-int  numV (Map g);
-int  numE (Map g, TransportID t);
+void disposeMap (Map m); 
+void showMap (Map m); 
+int  numV (Map m);
+int  numE (Map m, TransportID t);
 LocationID *BFS (Map m, LocationID src, LocationID dest, int *sizeArr);	//uses BFS to find shortest path from src to dest and stores in order
 																			//in returned array. size of array in sizeArr
 LocationID *DFS (Map m, LocationID src, LocationID dest, int *sizeArr); //same deal except use DFS. Include both maybe itll be good to use both
 //prototypes for possible future functions
 //int hasPathK (Map m, LocationID src, LocationID dest, int k); //uses some matrix multiplication to find if theres a path of length k. NOTE very expensive O(nV^2)
-=======
-Map  newMap();  
-void disposeMap(Map g); 
-void showMap(Map g); 
-int  numV(Map g);
-int  numE(Map g, TransportID t);
->>>>>>> Biendeo/master
-
-VList getConnections(Map g);
 
 #endif
