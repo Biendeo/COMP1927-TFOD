13c3a81fc6103464847058a288d767e144dbdfdf
siy34 <siy34@yahoo.com.au>
2015-09-25 20:55:00 +1000

    Added removeElement ()

--- Queue.h
+++ Queue.h
@@ -13,3 +13,4 @@ Trail newTrail (int size);
 void disposeTrail (Trail trail);
 void prepend (Trail trail, LocationID place);
 LocationID showElement (Trail trail, int index);
+void removeElement (Trail trail, LocationID place);
